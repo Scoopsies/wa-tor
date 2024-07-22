5c7245aeb8ac160305c51b7eb01ccfc3c1869310
Scoops Martin <michael.martin@cleancoders.com>
2024-07-22 12:18:23 -0500

    Refactored update board.

--- spec/wa_tor/update_board_spec.clj
+++ spec/wa_tor/update_board_spec.clj
@@ -37,35 +37,9 @@
     (let [pregnant-shark (assoc (c/create :shark [1 1])  :breeding 0)
           adjacent-spaces (shark/get-adjacent pregnant-shark)
           surrounded-list (concat [pregnant-shark] (map #(c/create :shark %) adjacent-spaces))]
-      (prn (count surrounded-list))
-      (should= 9 (count (sut/update-board surrounded-list))))))
+      (should= 9 (count (sut/update-board surrounded-list)))))
 
-
-#_(describe "update-board"
-  (context "update-board"
-    (it "removes any eaten fish"
-      (should= [:shark] (map :species (sut/update-board [(c/create :shark [0 0]) (c/create :fish [0 1])]))))
-
-    (it "doesn't remove surrounded creature"
-      (should=
-        (repeat 8 :shark)
-        (map :species
-             (sut/update-board
-               (conj (map #(c/create :shark %) (m/->adjacent-cells [1 1])) (c/create :shark [1 1]))))))
-
-    (it "doesn't include dead sharks"
-      (should= [] (sut/update-board [(assoc (c/create :shark [0 0]) :energy 0)])))
-
-    (it "adds a creature when breeding is 0"
-      (should= [:fish :fish] (map :species (sut/update-board [(assoc (c/create :fish [0 0]) :breeding 0)]))))
-
-    (it "decreases energy stat of shark if no fish eaten"
-      (should= [(dec s/shark-energy)] (map :energy (sut/update-board [(c/create :shark [0 0])]))))
-
-    (it "moves energy back to full if fish eaten"
-      (should= [s/shark-energy] (map :energy (sut/update-board [(c/create :shark [1 1]) (c/create :fish [0 0])] ))))
-
-    (it "decreases breeding stat of creature"
-      (should= [(dec s/fish-breeding)] (map :breeding (sut/update-board [(c/create :fish [0 0])]))))
-    )
+  (it "filters out dead sharks"
+    (let [shark (c/create :shark [1 1]) dead-shark (assoc (c/create :shark [1 2]) :energy 0)]
+      (should= [:shark] (map :species (sut/update-board [shark dead-shark])))))
   )
--- src/wa_tor/update_board.clj
+++ src/wa_tor/update_board.clj
@@ -1,69 +1,40 @@
 (ns wa-tor.update-board
   (:require [wa-tor.create :as c]
-            [wa-tor.move.shark :as shark]
-            [wa-tor.update-creature :as uc])
-  #_(:require [wa-tor.create :as c]
-            [wa-tor.move :as move]
-            [wa-tor.settings :as settings]))
+            [wa-tor.update-creature :as uc]))
 
-(defn filter-eaten-fish [old-list updated-creature]
-  (let [{:keys [position]} updated-creature]
-    (remove #(= position (:position %)) old-list)))
+(defn- filter-eaten-fish [old-list result]
+  (let [updated-creature (uc/update-creature (first old-list) (concat old-list result))
+        {:keys [position]} updated-creature]
+    (if (= position (:position (first old-list)))
+      old-list
+      (remove #(= position (:position %)) old-list))))
 
-(defn update-board [creature-list]
-  (loop [old-list creature-list result []]
-    (if (empty? old-list)
-      result
-      (let [current-creature (first old-list)
-            {:keys [species position]} current-creature
-            updated-creature (uc/update-creature current-creature old-list)]
-        (recur
-          (rest (filter-eaten-fish old-list updated-creature))
-          (if (zero? (:breeding current-creature))
-            (concat result [updated-creature (c/create species position)])
-            (conj (vec result) updated-creature)))))))
+(defn- in-labor? [current-creature updated-creature]
+  (let [{:keys [breeding position]} current-creature]
+    (and (zero? breeding) (not= position (:position updated-creature)))))
 
-;(defn remove-eaten [old-board updated-creature]
-;  (remove #(= (:position updated-creature) (:position %)) old-board))
-;
-;(defn dead? [old-creature]
-;  (= 0 (:energy old-creature)))
-;
-;(defn- pregnant? [old-creature]
-;  (= 0 (:breeding old-creature)))
-;
-;(defn- make-baby [old-creature updated-creature]
-;  (let [baby (c/create (:species old-creature) (:position old-creature))
-;        moved-creature (assoc updated-creature :breeding (:breeding baby))]
-;    [baby moved-creature]))
-;
-;(defn sharks-first [old-board]
-;  (sort-by #(= (:species %) :fish) old-board))
-;
-;(defmulti update-creature (fn [creature _] (:species creature)))
-;
-;(defmethod update-creature :fish [creature board]
-;  (assoc (move/move creature board) :breeding (dec (:breeding creature))))
-;
-;(defmethod update-creature :shark [creature board]
-;  (let [move (move/move creature board)
-;        fish-eaten? (some #(= (:position move) (:position %)) board)]
-;    (if fish-eaten?
-;      (assoc move :breeding (dec (:breeding creature)) :energy settings/shark-energy)
-;      (assoc move :breeding (dec (:breeding creature)) :energy (dec (:energy creature))))))
-;
-;(defn update-board [all-creatures]
-;  (loop [old-board all-creatures result []]
-;    (let [old-board (sharks-first old-board)]
-;      (if (empty? old-board)
-;        result
-;        (let [both-boards (concat old-board result)
-;              old-creature (first old-board)
-;              updated-creature (update-creature old-creature both-boards)]
-;          (recur
-;            (rest (remove-eaten old-board updated-creature))
-;            (cond
-;              (dead? old-creature) result
-;              (pregnant? old-creature) (concat result (make-baby old-creature updated-creature))
-;              :else (conj result updated-creature))))))))
-;
+(defn- make-baby [current-creature current-list]
+  (let [updated-creature (uc/update-creature current-creature current-list)
+        {:keys [species position]} current-creature]
+    [updated-creature (c/create species position)]))
+
+(defn- dead-shark? [current-creature]
+  (let [{:keys [species energy]} current-creature]
+    (and (= species :shark) (zero? energy))))
+
+(defn- handle-life-cycle [old-list result]
+  (let [current-creature (first old-list) current-list (concat old-list result)
+        updated-creature (uc/update-creature current-creature current-list)]
+    (cond
+      (dead-shark? current-creature) result
+      (in-labor? current-creature updated-creature) (concat result (make-baby current-creature current-list))
+      :else (conj (vec result) updated-creature))))
+
+(defn update-board
+  ([creature-list] (update-board creature-list []))
+
+  ([old-list result]
+   (if (empty? old-list)
+     result
+     (recur (rest (filter-eaten-fish old-list result))
+            (handle-life-cycle old-list result)))))
